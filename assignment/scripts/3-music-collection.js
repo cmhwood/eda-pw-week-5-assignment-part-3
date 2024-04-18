9c621aec948d9342840f3ec90d7f169200f3c6dc
Chris Harwood <cmharwood79@gmail.com>
2024-04-18 16:00:12 -0500

    display collection and find album by artist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -25,6 +25,40 @@ console.log(addToCollection(myCollection, 'Look Sharp', 'Roxette', 1988));
 console.log(addToCollection(myCollection, 'Spark', 'Drake White', 2016));
 console.log(addToCollection(myCollection, 'Baby Beluga', 'Raffi', 1980));
 
+// Function to display the collection
+function showCollection(collection) {
+  collection.forEach((album) => {
+    console.log(
+      `${album.title} by ${album.artist}, published in ${album.yearPublished}`
+    );
+  });
+}
+
+// Test showCollection function
+console.log('***** Collection *****');
+showCollection(myCollection);
+
+// Function to find albums by artist
+function findByArtist(collection, artist) {
+  const matchingAlbums = [];
+  collection.forEach((album) => {
+    if (album.artist === artist) {
+      matchingAlbums.push(album);
+    }
+  });
+  return matchingAlbums;
+}
+
+// Test findByArtist function
+const incubusAlbums = findByArtist(myCollection, 'Incubus');
+const drakeAlbums = findByArtist(myCollection, 'Drake White');
+
+console.log('***** Incubus Albums *****');
+showCollection(incubusAlbums);
+
+console.log('***** Drake White Albums *****');
+showCollection(drakeAlbums);
+
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
 // (It's used for automated testing.)
